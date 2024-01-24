843baeb91d85d91e10beb1916bea94312b098e34
@Lokie06 <lokyadav945275@gmail.com>
2024-01-25 00:15:19 +0530

    first commit

---
+++ src/controllers/article.controller.js
@@ -0,0 +1,81 @@
+import { Article } from "../models/article.model.js";
+import { asyncHandler } from "../utils/asynHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import axios from "axios";
+import { ApiError } from "../utils/ApiError.js";
+
+const postArticle = asyncHandler(async (req, res) => {
+  const { title, companyName, fullName, showName, description, tags, email } =
+    req.body;
+
+  const response = await axios.get(
+    `https://autocomplete.clearbit.com/v1/companies/suggest?query=${companyName}`
+  );
+
+  let companyDomainName;
+  if (response?.data?.length === 0) {
+    companyDomainName =
+      "https://cdn.pixabay.com/photo/2014/04/02/17/03/globe-307805_960_720.png";
+  } else {
+    companyDomainName = response.data[0].logo;
+  }
+
+  const article = await Article.create({
+    title,
+    companyName,
+    companyDomainName,
+    fullName,
+    showName,
+    description,
+    tags,
+    email,
+  });
+
+  // Verification
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, article, "Article Sent For Verification"));
+});
+
+const getAllArticles = asyncHandler(async (req, res) => {
+  const articles = await Article.find({ isVerified: true })
+    .sort({ _id: -1 })
+    .limit(10);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, articles, "All Articles Fetched"));
+});
+
+const getSingleArticle = asyncHandler(async (req, res) => {
+  const article = await Article.find({
+    _id: req.params.articleId,
+    isVerified: true,
+  }).select('-__v -companyDomainName -updatedAt -isVerified');
+
+  if (article.length === 0) {
+    throw new ApiError(400, `No article with found !!`);
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, article, "Read Full Article!!"));
+});
+
+const getArticleByTags = asyncHandler(async (req, res) => {
+  const tag = req.body.tags;
+  const article = await Article.find({
+    $and: [{ isVerified: true }, { tags: { $in: tag } }],
+  });
+
+  if (!article || article.length === 0) {
+    throw new ApiError(400, `No article with found By Tags!!`);
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, article, "Article Found by Tags!!"));
+});
+
+export { postArticle, getAllArticles, getSingleArticle, getArticleByTags };
---
+++ src/models/article.model.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+import { ValidationMiddleware } from "../middlewares/validation.middleware.js";
+
+const articleSchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    companyDomainName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: [true, "Email address is required"],
+      validate: {
+        validator: validator.isEmail,
+        message: "{VALUE} is not a valid email",
+        isAsync: false,
+      },
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: true,
+    },
+    showName: {
+      type: Boolean,
+      default: true,
+    },
+    tags: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+articleSchema.post(
+  "validate",
+  ValidationMiddleware(400, "Provide Valid Email")
+);
+export const Article = mongoose.model("Article", articleSchema);
